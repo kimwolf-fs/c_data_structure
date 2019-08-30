3d5caa9aa50cd3df9055e04db3e242b213a6e938
Kimwolf-Home <wzrde163@163.com>
2019-08-31 00:55:12 +0800

    从家同步代码

--- double_circular_linked_list/double_circular_linked_list.h
+++ double_circular_linked_list/double_circular_linked_list.h
@@ -1,39 +1,39 @@
 /* Define to prevent recursive inclusion -------------------------------------*/
-#ifndef __DOUBLECIRCULARLINKEDLIST_H
-#define __DOUBLECIRCULARLINKEDLIST_H
+#ifndef __DOUBLE_CIRCULAR_LINKED_LIST_H
+#define __DOUBLE_CIRCULAR_LINKED_LIST_H
 /* Includes ------------------------------------------------------------------*/
 /* Exported types ------------------------------------------------------------*/
 typedef int ElemType;      //数据元素的类型，假设是int型的
 
-typedef struct Node{
+struct double_circular_linked_list_node{
 	ElemType elem;  //存储空间
 	struct Node *prior;
 	struct Node *next;
-}Node;
+};
 
-typedef struct DoubleCircularLinkedList{
-	Node *This;
-	void (*clear)(struct DoubleCircularLinkedList *This);
-	int (*isEmpty)(struct DoubleCircularLinkedList *This);
-	int (*length)(struct DoubleCircularLinkedList *This);
-	void (*print)(struct DoubleCircularLinkedList *This);
-	void (*circlePrint)(struct DoubleCircularLinkedList *This,int times);
-	int (*indexElem)(struct DoubleCircularLinkedList *This, ElemType* x);
-	int (*indexNode)(struct DoubleCircularLinkedList *This, Node* n);
-	int (*getElem)(struct DoubleCircularLinkedList *This, int index, ElemType *e);
-	Node *(*getNode)(struct DoubleCircularLinkedList *This, int index);
-	Node *(*getPriorNode)(Node *n);
-	Node *(*getNextNode)(Node *n);
-	int (*modifyElem)(struct DoubleCircularLinkedList *This, int index, ElemType* e);
-	int (*deleteElem)(struct DoubleCircularLinkedList *This, int index, ElemType* e);
-	int (*deleteNode)(struct DoubleCircularLinkedList *This, Node* n);
-	int (*appendElem)(struct DoubleCircularLinkedList *This, ElemType *e);
-	int (*insertElem)(struct DoubleCircularLinkedList *This, int index, ElemType *e);
-	int (*popElem)(struct DoubleCircularLinkedList *This, ElemType* e);
-}DoubleCircularLinkedList;
+struct double_circular_linked_list{
+	double_circular_linked_list_node *This;
+	void (*clear)(struct double_circular_linked_list *This);
+	int (*isEmpty)(struct double_circular_linked_list *This);
+	int (*length)(struct double_circular_linked_list *This);
+	void (*print)(struct double_circular_linked_list *This);
+	void (*circlePrint)(struct double_circular_linked_list *This,int times);
+	int (*indexElem)(struct double_circular_linked_list *This, ElemType* x);
+	int (*indexNode)(struct double_circular_linked_list *This, double_circular_linked_list_node* n);
+	int (*getElem)(struct double_circular_linked_list *This, int index, ElemType *e);
+	double_circular_linked_list_node *(*getNode)(struct double_circular_linked_list *This, int index);
+	double_circular_linked_list_node *(*getPriorNode)(double_circular_linked_list_node *n);
+	double_circular_linked_list_node *(*getNextNode)(double_circular_linked_list_node *n);
+	int (*modifyElem)(struct double_circular_linked_list *This, int index, ElemType* e);
+	int (*deleteElem)(struct double_circular_linked_list *This, int index, ElemType* e);
+	int (*deleteNode)(struct double_circular_linked_list *This, Node* n);
+	int (*appendElem)(struct double_circular_linked_list *This, ElemType *e);
+	int (*insertElem)(struct double_circular_linked_list *This, int index, ElemType *e);
+	int (*popElem)(struct double_circular_linked_list *This, ElemType* e);
+};
 
 /* Exported macro ------------------------------------------------------------*/
-DoubleCircularLinkedList *InitDoubleCircularLinkedList();
-void DestroyDoubleCircularLinkedList(DoubleCircularLinkedList *L);
+double_circular_linked_list *InitDoubleCircularLinkedList();
+void destroy_double_circular_linked_list(DoubleCircularLinkedList *L);
 
 #endif
