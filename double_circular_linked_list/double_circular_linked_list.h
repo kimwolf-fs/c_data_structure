4cbb4c35f756e53941a25e17ca4e8f2106223927
WangZherenForTABR <wzrde163@163.com>
2019-08-31 17:41:44 +0800

    从公司同步代码

--- double_circular_linked_list/double_circular_linked_list.h
+++ double_circular_linked_list/double_circular_linked_list.h
@@ -12,7 +12,7 @@ struct double_circular_linked_list_node{
 };
 
 struct double_circular_linked_list{
-	double_circular_linked_list_node *This;
+    struct double_circular_linked_list_node *This;
 	void (*clear)(struct double_circular_linked_list *This);
 	int (*isEmpty)(struct double_circular_linked_list *This);
 	int (*length)(struct double_circular_linked_list *This);
@@ -21,9 +21,9 @@ struct double_circular_linked_list{
 	int (*indexElem)(struct double_circular_linked_list *This, ElemType* x);
 	int (*indexNode)(struct double_circular_linked_list *This, double_circular_linked_list_node* n);
 	int (*getElem)(struct double_circular_linked_list *This, int index, ElemType *e);
-	double_circular_linked_list_node *(*getNode)(struct double_circular_linked_list *This, int index);
-	double_circular_linked_list_node *(*getPriorNode)(double_circular_linked_list_node *n);
-	double_circular_linked_list_node *(*getNextNode)(double_circular_linked_list_node *n);
+    struct double_circular_linked_list_node *(*getNode)(struct double_circular_linked_list *This, int index);
+    struct double_circular_linked_list_node *(*getPriorNode)(double_circular_linked_list_node *n);
+    struct double_circular_linked_list_node *(*getNextNode)(double_circular_linked_list_node *n);
 	int (*modifyElem)(struct double_circular_linked_list *This, int index, ElemType* e);
 	int (*deleteElem)(struct double_circular_linked_list *This, int index, ElemType* e);
 	int (*deleteNode)(struct double_circular_linked_list *This, Node* n);
